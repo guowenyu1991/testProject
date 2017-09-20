fa2316af73537ef26004558cfd9471679a9b01aa
guowenyu1991 <2804285253@qq.com>
2017-09-20 23:49:43 +0800

    add sysout row

--- TestVue/src/cn/extend/resource/MyTest.java
+++ TestVue/src/cn/extend/resource/MyTest.java
@@ -3,11 +3,6 @@ package cn.extend.resource;
 public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("This is guowenyu project!!!");
-		
-		
-		
-		
-		
-		
+		System.out.println("add sysout row");
 	}
 }
