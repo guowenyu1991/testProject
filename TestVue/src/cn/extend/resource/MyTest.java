8ad959749b68196c6ed6359fcd3d6adfe5a1c20c
guowenyu1991 <2804285253@qq.com>
2017-09-20 23:52:42 +0800

    i am here

--- TestVue/src/cn/extend/resource/MyTest.java
+++ TestVue/src/cn/extend/resource/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("This is guowenyu project!!!");
 		System.out.println("add sysout row");
+		System.out.println("i am here");
 	}
 }
